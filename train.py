5b9a7609589eca0908273ec7680030765a0ff027
Hang Le <hangtp.le@gmail.com>
2019-12-10 21:11:52 +0100

    Fixed blank line (tested Hang's account)

--- train.py
+++ train.py
@@ -7,7 +7,6 @@ All rights reserved.
 This source code is licensed under the license found in the
 LICENSE file in the root directory of this source tree.
 """
-
 import json
 import random
 import argparse
