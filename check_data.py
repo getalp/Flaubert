89d99788a12f8698d5cbc6b414ba8e28499f8819
Hang Le <hangtp.le@gmail.com>
2020-01-18 15:04:57 +0100

    Removed unneccessary files

--- check_data.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-def main():
-    path = '/home/getalp/lethip/Data/BERT-FR/data_processed/combined_corpus_nc/BPE/50k/train.fr.0.pth'
-    data = torch.load(path)
-
-if __name__ == "__main__":
-    main()
